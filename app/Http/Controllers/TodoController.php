25f7406b6e7f689a26411f6b7df4524d3f2b5f37
saber Mohammed <sabersss9@gamil.com>
2020-07-21 14:00:59 +0300

    laravel todos

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+        $todos  = Todo::all();
+        return view('todos.index')->with([
+            'todos' => $todos,
+        ]);;
+    }
+}
---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <title>Todos</title>
+    </head>
+    <body>
+        <h1 class ="text-center my-5"> Todos Pages </h1>
+        <div class = " row justify-content-center">
+            <div class = "col-md-8 ">
+                <div class="card card-default">
+                    <div class="card-header">
+                      todos
+                    </div>
+                   <div class="card-body">
+                        <ul class="list-group ">
+                            @foreach ( $todos as $todo)
+                                <li class="list-group-item">{{ $todo->name }}</li>
+                            @endforeach
+                        </ul>
+                   </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
